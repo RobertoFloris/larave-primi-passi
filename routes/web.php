d39f17e13b4c7a301ee12b0f84943c6905ea5425
RobertoFloris <florisroberto99@gmail.com>
2025-04-01 16:23:16 +0200

    fix route

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,17 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    $dati="dati dinamici";
+    $dati="Siamo nella home";
     return view('home', compact("dati"));
-});
+})->name("home");
+
+Route::get('/features', function () {
+    $dati="Siamo nella features";
+    return view('home', compact("dati"));
+})->name("features");
+
+
+Route::get('/pricing', function () {
+    $dati="Siamo nella pricing";
+    return view('home', compact("dati"));
+})->name("pricing");;
